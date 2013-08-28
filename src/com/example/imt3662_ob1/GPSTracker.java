02e6602af2bd1b9da883d5013070edb9d0eaa105
pimms <joakim.stien@gmail.com>
2013-08-28 14:31:56 +0200

    Added basic functionality for retrieving positional data.

---
+++ src/com/example/imt3662_ob1/GPSTracker.java
@@ -0,0 +1,94 @@
+package com.example.imt3662_ob1;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+public class GPSTracker extends Service implements LocationListener {
+	
+	boolean gpsEnabled = false;
+	boolean netEnabled = false;
+	
+	LocationManager locationManager;
+	Location location;
+	
+	private Context context;
+	private boolean locSubscription = false;
+	
+	public GPSTracker(Context ctx) {
+		context = ctx;
+		locationManager = (LocationManager)context.getSystemService(LOCATION_SERVICE);
+		
+		if (servicesAvailable()) {
+			subscribeLocation();
+		}
+	}
+	
+	public boolean servicesAvailable() {
+		gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		netEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+		
+		return (gpsEnabled && netEnabled);
+	}
+	
+	public Location getLocation() {
+		if (!servicesAvailable()) {
+			Log.e("DBG", "GPS Services unavailable :(");
+			return null;
+		} 
+		
+		if (!locSubscription) {
+			subscribeLocation();
+		}
+		
+		Location tmpLocation;
+		tmpLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		
+		if (tmpLocation != null) {
+			location = tmpLocation;
+		} else {
+			Log.e("DBG", "Last known location is null :(");
+		}
+		
+		return location;
+	}
+	
+	
+	private void subscribeLocation() {
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 10, this);
+		locSubscription = true;
+	}
+	
+	
+	@Override
+	public void onLocationChanged(Location arg0) {
+		Log.e("DBG", "Location changed!!!!!!!!");
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+
+}
--- src/com/example/imt3662_ob1/MainActivity.java
+++ src/com/example/imt3662_ob1/MainActivity.java
@@ -1,15 +1,50 @@
 package com.example.imt3662_ob1;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.util.Log;
+import android.view.*;
+import android.widget.Button;
+import android.widget.TextView;
+import android.app.*;
+import android.content.Context;
 
 public class MainActivity extends Activity {
+	
+	GPSTracker gpsTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        gpsTracker = new GPSTracker(this);
+        
+        initButton();
+    }
+    
+    protected void initButton() {
+    	final Button button = (Button)findViewById(R.id.button_get_loc);
+    	final TextView tvLat = (TextView)findViewById(R.id.textViewLatitude);
+    	final TextView tvLon = (TextView)findViewById(R.id.textViewLongitude);
+    	
+    	button.setOnClickListener(new View.OnClickListener() {
+    		public void onClick(View v) {
+    			Location location = gpsTracker.getLocation();
+    			if (location == null) {
+    				return;
+    			}
+    			
+    			StringBuilder str = new StringBuilder();
+    			str.append(location.getLatitude());
+    			tvLat.setText(str.toString());
+    			
+    			str.delete(0, 100);
+    			str.append(location.getLongitude());
+    			tvLon.setText(str.toString());
+    		}
+    	});
     }
 
 
